a7b9566a2c03e25def7fc00f4618a319d0c0855d
TBSten <81161390+TBSten@users.noreply.github.com>
2024-03-17 10:00:07 +0900

    slack compose-lintsを導入

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,6 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    lintChecks("com.slack.lint.compose:compose-lint-checks:1.3.1")
 }
